40ce7795e795dbc8d63095c1d64073ab9e32dcaf
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-05-27 17:14:06 -0300

    criando componente edit

---
+++ src/components/Edit/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './edit.css';
+
+function Edit(){
+    return(
+        <div className="edit">
+            <span className="edit-btn">Editar meu perfil</span>
+        </div>
+    )
+}
+
+export default Edit;
