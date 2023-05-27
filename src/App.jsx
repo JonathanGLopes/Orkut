75d1511422385350f8671d71f835c512bb53b852
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-05-27 17:14:16 -0300

    importando componente edit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import Header from './components/Header';
 import Perfil from './components/Perfil';
+import Edit from './components/Edit';
 
 function App() {
   return (
     <>
       <Header />
       <Perfil />
+      <Edit />
     </>
   )
 }
