841817f2c90ace7fb03d60e52e5e46d65b3df947
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-06-03 16:31:56 -0300

    importando componente description

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Perfil from './components/Perfil';
 import Edit from './components/Edit';
 import Friends from './components/Friends';
+import Description from './components/Description';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Perfil />
       <Edit />
       <Friends />
+      <Description />
     </>
   )
 }
