75fdec886c83024b5726544191071dea0d4109c8
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-06-28 23:37:54 -0300

    Criando o componente Right e inserindo nele os componentes Friends e Community

---
+++ src/components/Right/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Friends from "../Friends";
+import Community from "../Community";
+
+export default function Right(){
+    return(
+        <>
+            <Friends />
+            <Community />
+        </>
+    )
+}
