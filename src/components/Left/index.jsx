4d6bb8421376edc33862e1c83def1a57d698058e
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-06-28 23:36:58 -0300

    Criando componente Left e inserindo dentro dele os componentes Perfil e Edit

---
+++ src/components/Left/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import './Left.css';
+import Perfil from "../Perfil";
+import Edit from "../Edit";
+
+export default function Left(){
+    return(
+        <>
+            <Perfil />
+            <Edit />
+        </>
+    )
+}
