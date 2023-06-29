eb28a8639c72d0a00f28731df81cdc848f7d6fcc
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-06-28 23:38:07 -0300

    importando novos componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import React from 'react'
 import './App.css'
 import Header from './components/Header';
-import Perfil from './components/Perfil';
-import Edit from './components/Edit';
-import Friends from './components/Friends';
-import Description from './components/Description';
-import Community from './components/Community';
+import Left from './components/Left';
+import Right from './components/Right';
+import Main from './components/Main';
 
 function App() {
   return (
     <>
       <Header />
-      <Perfil />
-      <Edit />
-      <Description />
-      <Friends />
-      <Community />
+      <Left />
+      <Main />
+      <Right />
     </>
   )
 }
