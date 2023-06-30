3bbec7018ce6c8234152c30e306e69aa70384f60
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-06-29 22:40:15 -0300

    importando componente Wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import './App.css'
 import Header from './components/Header';
-import Left from './components/Left';
-import Right from './components/Right';
-import Main from './components/Main';
+import Wrapper from './components/Wrapper';
 
 function App() {
   return (
     <>
       <Header />
-      <Left />
-      <Main />
-      <Right />
+      <Wrapper />
     </>
   )
 }
