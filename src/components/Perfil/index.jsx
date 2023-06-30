f1dc694a00851977f24cec8024331a9d24914b7c
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-06-29 22:49:15 -0300

    Css dos componentes para desktop e importação

--- src/components/Perfil/index.jsx
+++ src/components/Perfil/index.jsx
@@ -3,14 +3,15 @@ import './perfil.css';
 
 import imgPerfil from '../../img/icones/imgperfil.jpg';
 import Card from "../Card";
+import Edit from "../Edit";
 
 function Perfil(){
     return(
         <Card className="perfil-card">
-            <div className="border"></div>
             <img src={imgPerfil} alt="" className="perfil-img" />
             <span className="perfil-name">Jonathan Lopes</span>
             <span className="perfil-status">Solteiro, Brasil</span>
+            <Edit />
         </Card>
     )
 }
--- src/components/Right/index.jsx
+++ src/components/Right/index.jsx
@@ -4,9 +4,9 @@ import Community from "../Community";
 
 export default function Right(){
     return(
-        <>
+        <div>
             <Friends />
             <Community />
-        </>
+        </div>
     )
 }
